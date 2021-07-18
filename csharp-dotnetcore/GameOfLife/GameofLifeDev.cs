a87251dd502e970919ebbfc70a4ecf7230c49b0c
K Johny, Sajitha <sajitha.k.johny@accenture.com>
2021-07-18 21:13:00 +0530

    GameOfLife Code added

---
+++ csharp-dotnetcore/GameOfLife/GameofLifeDev.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameOfLife
+{
+    public class GameofLifeDev
+    {        
+        static public void Main(String[] args)
+        {
+
+        }
+        public Cell PlayGame(Cell currentCell,List<Cell> neigbourcells)
+        {
+            int deadStateCount = 0;
+         
+            foreach (Cell data in neigbourcells)
+            {
+                if(data.CellState == "Dead")
+                {
+                    deadStateCount ++;
+                }
+            }
+            if((neigbourcells.Count - deadStateCount) < 2)
+            {
+                currentCell.CellState = "Dead";
+                return currentCell;
+            }
+            else if ((neigbourcells.Count - deadStateCount) == 2 || (neigbourcells.Count - deadStateCount) == 3)
+            {
+                currentCell.CellState = "Live";
+                return currentCell;
+            }
+            else if ((neigbourcells.Count - deadStateCount) > 3) 
+            {
+                currentCell.CellState = "Dead";
+                return currentCell;
+            }
+            return currentCell;
+        }
+
+        public Cell PlayBoardGame(Cell currentCell, Cell[,] board,int rowMax,int columnMax)
+        {
+
+            List<Cell> neighbours = new List<Cell>();
+            int deadStateCount = 0;
+            int liveStateCount = 0;
+            Cell neighbour = new Cell();
+            bool loopConditionContinue= false;
+            for (int x = 0; x <= rowMax; x++)
+            {
+                if(loopConditionContinue)
+                {
+                   break;
+                }
+                for (int y = 0; y <= columnMax; y++)
+                {
+                    if (currentCell.CellPositionX == x && currentCell.CellPositionY == y)
+                    {
+                        if (x < rowMax && y <= columnMax - 1)
+                        {
+                            neighbours.Add((Cell)board.GetValue(x, y + 1));
+                        }
+                        if (x < rowMax && y < columnMax)
+                        {
+                            neighbours.Add((Cell)board.GetValue(x + 1, y));
+                            neighbours.Add((Cell)board.GetValue(x + 1, y + 1));
+                        }
+                        if (x <= rowMax && y <= columnMax && y != 0)
+                        {
+                            neighbours.Add((Cell)board.GetValue(x, y - 1));
+                        }
+                        if (x < rowMax && y <= columnMax && y != 0)
+                        {
+                            neighbours.Add((Cell)board.GetValue(x + 1, y - 1));
+                        }
+                        if (x <= rowMax && y <= columnMax && y != 0 && x != 0)
+                        {
+                            neighbours.Add((Cell)board.GetValue(x - 1, y - 1));
+                        }
+                        if (x <= rowMax && y < columnMax  && x != 0)
+                        {
+                            neighbours.Add((Cell)board.GetValue(x - 1, y));
+                        }
+                        if (x <= rowMax && y <= columnMax - 1 && x != 0)
+                        {
+                            neighbours.Add((Cell)board.GetValue(x - 1, y + 1));
+                        }
+
+                        loopConditionContinue = true;
+                        break;
+                    }
+
+
+                }
+            }
+
+           
+            foreach (Cell data in neighbours)
+            {
+                if (data.CellState == "Dead")
+                {
+                    deadStateCount++;
+                }
+            }
+
+            liveStateCount = neighbours.Count - deadStateCount;
+
+            //Any live cell with less than two live neighbours dies, as if by underpopulation.
+            if (currentCell.CellState == "Live" && liveStateCount < 2)
+            {
+                currentCell.CellState = "Dead";
+                return currentCell;
+            }
+            //Any live cell with two or three live neighbours lives on to the next generation.
+            else if ((currentCell.CellState == "Live")  && (liveStateCount == 2 || liveStateCount == 3))
+            {
+                currentCell.CellState = "Live";
+                return currentCell;
+            }           
+            //Any live cell with more than three live neighbours dies, as if by overpopulation.
+            else if ((currentCell.CellState == "Live") && (liveStateCount > 3))
+            {
+                currentCell.CellState = "Dead";
+                return currentCell;
+            }
+            //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+            else if ((currentCell.CellState == "Dead") && (liveStateCount == 3))
+            {
+                currentCell.CellState = "Live";
+                return currentCell;
+            }
+            return currentCell;
+        }
+    }
+}
---
+++ csharp-dotnetcore/GameOfLifeTests/GameOfLifeTest.cs
@@ -0,0 +1,234 @@
+﻿using FluentAssertions;
+using GameOfLife;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GameOfLifeTests
+{
+    public class GameOfLifeTest
+    {
+        [Fact]
+        public void testUnderpopulation()
+        {
+
+            Cell currentCell = new Cell() { CellState = "Live" };
+            Cell cell2 = new Cell() { CellState = "Live" };
+            Cell cell3 = new Cell() { CellState = "Dead" };
+            Cell cell4 = new Cell() { CellState = "Dead" };
+            List<Cell> neighbours = new List<Cell>() { cell2, cell3, cell4 };
+            GameofLifeDev gameoflife = new GameofLifeDev();
+            gameoflife.PlayGame(currentCell, neighbours).CellState.Should().NotBe("Live");
+
+        }
+
+        [Fact]
+        public void testNextGeneration_withneighbour2Live()
+        {
+            Cell currentCell = new Cell() { CellState = "Live" };
+            Cell cell2 = new Cell() { CellState = "Dead" };
+            Cell cell3 = new Cell() { CellState = "Live" };
+            Cell cell4 = new Cell() { CellState = "Live" };
+            List<Cell> neighbours = new List<Cell>() { cell2, cell3, cell4 };
+
+            GameofLifeDev gameoflife = new GameofLifeDev();
+            gameoflife.PlayGame(currentCell, neighbours).CellState.Should().Be("Live");
+
+        }
+        [Fact]
+        public void testNextGeneration__withneighbour3Live()
+        {
+            Cell currentCell = new Cell() { CellState = "Live" };
+            Cell cell2 = new Cell() { CellState = "Live" };
+            Cell cell3 = new Cell() { CellState = "Live" };
+            Cell cell4 = new Cell() { CellState = "Live" };
+            List<Cell> neighbours = new List<Cell>() { cell2, cell3, cell4 };
+
+            GameofLifeDev gameoflife = new GameofLifeDev();
+            gameoflife.PlayGame(currentCell, neighbours).CellState.Should().Be("Live");
+
+        }
+
+        [Fact]
+        public void testOverPopulation()
+        {
+            Cell currentCell = new Cell() { CellState = "Live" };
+            Cell cell2 = new Cell() { CellState = "Live" };
+            Cell cell3 = new Cell() { CellState = "Live" };
+            Cell cell4 = new Cell() { CellState = "Live" };
+            Cell cell5 = new Cell() { CellState = "Live" };
+            List<Cell> neighbours = new List<Cell>() { cell2, cell3, cell4, cell5 };
+
+            GameofLifeDev gameoflife = new GameofLifeDev();
+            gameoflife.PlayGame(currentCell, neighbours).CellState.Should().Be("Dead");
+
+        }
+
+        [Fact]
+        public void testReproduction()
+        {
+            Cell currentCell = new Cell() { CellState = "Dead" };
+            Cell cell2 = new Cell() { CellState = "Live" };
+            Cell cell3 = new Cell() { CellState = "Live" };
+            Cell cell4 = new Cell() { CellState = "Live" };
+            List<Cell> neighbours = new List<Cell>() { cell2, cell3, cell4 };
+
+            GameofLifeDev gameoflife = new GameofLifeDev();
+            gameoflife.PlayGame(currentCell, neighbours).CellState.Should().Be("Live");
+
+        }
+        [Fact]
+        public void testUnderpopulationforCurrentCellLive_WithFewerThanTwoLiveofNeighboursDies()
+        {
+            Cell currentCell = new Cell() { CellState = "Live", CellPositionX = 0, CellPositionY = 0 };
+
+            Cell cellx0y0 = new Cell() { CellState = "Live", CellPositionX = 0, CellPositionY = 0 };
+            Cell cellx0y1 = new Cell() { CellState = "Dead", CellPositionX = 0, CellPositionY = 1 };
+            Cell cellx0y2 = new Cell() { CellState = "Dead", CellPositionX = 0, CellPositionY = 2 };
+            Cell cellx1y0 = new Cell() { CellState = "Dead", CellPositionX = 1, CellPositionY = 0 };
+            Cell cellx1y1 = new Cell() { CellState = "Live", CellPositionX = 1, CellPositionY = 1 };
+            Cell cellx1y2 = new Cell() { CellState = "Live", CellPositionX = 1, CellPositionY = 2 };
+            Cell cellx2y0 = new Cell() { CellState = "Live", CellPositionX = 2, CellPositionY = 0 };
+            Cell cellx2y1 = new Cell() { CellState = "Live", CellPositionX = 2, CellPositionY = 1 };
+            Cell cellx2y2 = new Cell() { CellState = "Live", CellPositionX = 2, CellPositionY = 2 };
+
+            int rowMax = 3;
+            int columnMax = 3;
+
+            Cell[,] board = new Cell[3, 3]
+            {
+             {cellx0y0, cellx0y1, cellx0y2},
+             {cellx1y0, cellx1y1, cellx1y2},
+             {cellx2y0, cellx2y1, cellx2y2}
+            };
+
+            GameofLifeDev gameoflife = new GameofLifeDev();
+            gameoflife.PlayBoardGame(currentCell, board, rowMax, columnMax).CellState.Should().NotBe("Live"); ;
+
+        }
+       
+        [Fact]
+        public void testNextGenerationforCurrentCellLive_WithTw0LiveofNeighboursLive()
+        {
+            Cell currentCell = new Cell() { CellState = "Live", CellPositionX = 0, CellPositionY = 0 };
+
+            Cell cellx0y0 = new Cell() { CellState = "Live", CellPositionX = 0, CellPositionY = 0 };
+            Cell cellx0y1 = new Cell() { CellState = "Live", CellPositionX = 0, CellPositionY = 1 };
+            Cell cellx0y2 = new Cell() { CellState = "Dead", CellPositionX = 0, CellPositionY = 2 };
+            Cell cellx1y0 = new Cell() { CellState = "Live", CellPositionX = 1, CellPositionY = 0 };
+            Cell cellx1y1 = new Cell() { CellState = "Dead", CellPositionX = 1, CellPositionY = 1 };
+            Cell cellx1y2 = new Cell() { CellState = "Dead", CellPositionX = 1, CellPositionY = 2 };
+            Cell cellx2y0 = new Cell() { CellState = "Dead", CellPositionX = 2, CellPositionY = 0 };
+            Cell cellx2y1 = new Cell() { CellState = "Dead", CellPositionX = 2, CellPositionY = 1 };
+            Cell cellx2y2 = new Cell() { CellState = "Dead", CellPositionX = 2, CellPositionY = 2 };
+
+            int rowMax = 3;
+            int columnMax = 3;
+
+            Cell[,] board = new Cell[3, 3]
+            {
+             {cellx0y0, cellx0y1, cellx0y2},
+             {cellx1y0, cellx1y1, cellx1y2},
+             {cellx2y0, cellx2y1, cellx2y2}
+            };
+
+            GameofLifeDev gameoflife = new GameofLifeDev();
+            gameoflife.PlayBoardGame(currentCell, board, rowMax, columnMax).CellState.Should().Be("Live"); 
+
+        }
+
+
+        [Fact]
+        public void testNextGenerationforCurrentCellLive_WithThreeLiveofNeighboursLive()
+        {
+            Cell currentCell = new Cell() { CellState = "Live", CellPositionX = 0, CellPositionY = 0 };
+
+            Cell cellx0y0 = new Cell() { CellState = "Live", CellPositionX = 0, CellPositionY = 0 };
+            Cell cellx0y1 = new Cell() { CellState = "Live", CellPositionX = 0, CellPositionY = 1 };
+            Cell cellx0y2 = new Cell() { CellState = "Dead", CellPositionX = 0, CellPositionY = 2 };
+            Cell cellx1y0 = new Cell() { CellState = "Live", CellPositionX = 1, CellPositionY = 0 };
+            Cell cellx1y1 = new Cell() { CellState = "Live", CellPositionX = 1, CellPositionY = 1 };
+            Cell cellx1y2 = new Cell() { CellState = "Dead", CellPositionX = 1, CellPositionY = 2 };
+            Cell cellx2y0 = new Cell() { CellState = "Dead", CellPositionX = 2, CellPositionY = 0 };
+            Cell cellx2y1 = new Cell() { CellState = "Dead", CellPositionX = 2, CellPositionY = 1 };
+            Cell cellx2y2 = new Cell() { CellState = "Dead", CellPositionX = 2, CellPositionY = 2 };
+
+            int rowMax = 3;
+            int columnMax = 3;
+
+            Cell[,] board = new Cell[3, 3]
+            {
+             {cellx0y0, cellx0y1, cellx0y2},
+             {cellx1y0, cellx1y1, cellx1y2},
+             {cellx2y0, cellx2y1, cellx2y2}
+            };
+
+            GameofLifeDev gameoflife = new GameofLifeDev();
+            gameoflife.PlayBoardGame(currentCell, board, rowMax, columnMax).CellState.Should().Be("Live");
+
+        }
+
+       
+        [Fact]
+        public void testOverPopulationforCurrentCellLive_WithMoreThanThreeLiveofNeighboursLive()
+        {
+            Cell currentCell = new Cell() { CellState = "Live", CellPositionX = 1, CellPositionY = 1 };
+
+            Cell cellx0y0 = new Cell() { CellState = "Live", CellPositionX = 0, CellPositionY = 0 };
+            Cell cellx0y1 = new Cell() { CellState = "Live", CellPositionX = 0, CellPositionY = 1 };
+            Cell cellx0y2 = new Cell() { CellState = "Live", CellPositionX = 0, CellPositionY = 2 };
+            Cell cellx1y0 = new Cell() { CellState = "Live", CellPositionX = 1, CellPositionY = 0 };
+            Cell cellx1y1 = new Cell() { CellState = "Live", CellPositionX = 1, CellPositionY = 1 };
+            Cell cellx1y2 = new Cell() { CellState = "Live", CellPositionX = 1, CellPositionY = 2 };
+            Cell cellx2y0 = new Cell() { CellState = "Live", CellPositionX = 2, CellPositionY = 0 };
+            Cell cellx2y1 = new Cell() { CellState = "Live", CellPositionX = 2, CellPositionY = 1 };
+            Cell cellx2y2 = new Cell() { CellState = "Live", CellPositionX = 2, CellPositionY = 2 };
+
+            int rowMax = 3;
+            int columnMax = 3;
+
+            Cell[,] board = new Cell[3, 3]
+            {
+             {cellx0y0, cellx0y1, cellx0y2},
+             {cellx1y0, cellx1y1, cellx1y2},
+             {cellx2y0, cellx2y1, cellx2y2}
+            };
+
+            GameofLifeDev gameoflife = new GameofLifeDev();
+            gameoflife.PlayBoardGame(currentCell, board, rowMax, columnMax).CellState.Should().Be("Dead");
+
+        }
+
+        
+        [Fact]
+        public void testReproductionforCurrentCellDead_WithexactlyThreeLiveofNeighboursLive()
+        {
+            Cell currentCell = new Cell() { CellState = "Dead", CellPositionX = 0, CellPositionY = 0 };
+
+            Cell cellx0y0 = new Cell() { CellState = "Dead", CellPositionX = 0, CellPositionY = 0 };
+            Cell cellx0y1 = new Cell() { CellState = "Live", CellPositionX = 0, CellPositionY = 1 };
+            Cell cellx0y2 = new Cell() { CellState = "Dead", CellPositionX = 0, CellPositionY = 2 };
+            Cell cellx1y0 = new Cell() { CellState = "Live", CellPositionX = 1, CellPositionY = 0 };
+            Cell cellx1y1 = new Cell() { CellState = "Live", CellPositionX = 1, CellPositionY = 1 };
+            Cell cellx1y2 = new Cell() { CellState = "Dead", CellPositionX = 1, CellPositionY = 2 };
+            Cell cellx2y0 = new Cell() { CellState = "Dead", CellPositionX = 2, CellPositionY = 0 };
+            Cell cellx2y1 = new Cell() { CellState = "Dead", CellPositionX = 2, CellPositionY = 1 };
+            Cell cellx2y2 = new Cell() { CellState = "Dead", CellPositionX = 2, CellPositionY = 2 };
+
+            int rowMax = 3;
+            int columnMax = 3;
+
+            Cell[,] board = new Cell[3, 3]
+            {
+             {cellx0y0, cellx0y1, cellx0y2},
+             {cellx1y0, cellx1y1, cellx1y2},
+             {cellx2y0, cellx2y1, cellx2y2}
+            };
+
+            GameofLifeDev gameoflife = new GameofLifeDev();
+            gameoflife.PlayBoardGame(currentCell, board, rowMax, columnMax).CellState.Should().Be("Live");
+
+        }
+
+
+    }
+}
